293b7c5d59c77fc6b98513dd8935559014ae482b
noaione <aimanmh@my.smccd.edu>
2019-02-08 13:26:42 +0700

    Version 0.1.4.1 - Added some support for `channel` or `slots` url things - Make some auto-output-parser thingy for direct m3u8 link - Cleaning some code

--- yuu/common.py
+++ yuu/common.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.4.1'
 
 STRTABLE = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 HKEY = b"3AF0298C219469522A313570E8583005A642E73EDD58E3EA2FB7339D3DF1597E"
@@ -16,3 +16,11 @@ _MEDIATOKEN_API = "https://api.abema.io/v1/media/token"
 _LICENSE_API = "https://license.abema.io/abematv-hls"
 _USERAPI = "https://api.abema.io/v1/users"
 _PROGRAMAPI = 'https://api.abema.io/v1/video/programs/'
+_CHANNELAPI = 'https://api.abema.io/v1/media/slots/'
+
+def is_channel(url):
+	url = url[:url.rfind('/')]
+	url = url[url.rfind('/')+1:]
+	if url == "slots":
+		return True
+	return False
