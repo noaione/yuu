56e6e74243bcbad315c762f20e9a0099e53100f6
NoAiOne <aimanmh@my.smccd.edu>
2019-07-03 22:37:16 +0700

    Version 1.5.0
    - Tabs -> Spaces Indentation
    - Add `--resolutions` for showing list of available resolution
    - Some fixes for the API changes

--- yuu/common.py
+++ yuu/common.py
@@ -1,17 +1,17 @@
 import re
 
-__version__ = '0.1.4.3'
+__version__ = '0.1.5'
 
 _STRTABLE = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 _HKEY = b"3AF0298C219469522A313570E8583005A642E73EDD58E3EA2FB7339D3DF1597E"
 
 _KEYPARAMS = {
-	"osName": "android",
-	"osVersion": "6.0.1",
-	"osLand": "ja_JP",
-	"osTimezone": "Asia/Tokyo",
-	"appId": "tv.abema",
-	"appVersion": "3.27.1"
+    "osName": "android",
+    "osVersion": "6.0.1",
+    "osLand": "ja_JP",
+    "osTimezone": "Asia/Tokyo",
+    "appId": "tv.abema",
+    "appVersion": "3.27.1"
 }
 
 _MEDIATOKEN_API = "https://api.abema.io/v1/media/token"
@@ -21,7 +21,7 @@ _PROGRAMAPI = 'https://api.abema.io/v1/video/programs/'
 _CHANNELAPI = 'https://api.abema.io/v1/media/slots/'
 
 def is_channel(url):
-	url = re.findall('(slots)', url)
-	if not url:
-		return True
-	return False
+    url = re.findall('(slots)', url)
+    if url:
+        return True
+    return False
