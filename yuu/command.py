8e69c14f8fe415bf120ab8219f97aec748fddf88
NoAiOne <aimanmh@my.smccd.edu>
2019-07-19 07:08:56 +0700

    Rewrite Progress 1
    - Rewrite command to utilize multiple parser
    - Rewrite abematv parser to class
    - Start making GYAO parser
    - Simplify downloader
    - Modify save folder to:
    	- Windows: AppData/Local/yuu_data
    	- Linux: ~/.yuu_data

--- yuu/command.py
+++ yuu/command.py
@@ -6,9 +6,8 @@ import click
 import requests
 
 from .common import __version__, isUserAdmin, res_data
-from .downloader import get_video, merge_video
-from .parser import (available_resolution, fetch_video_key, get_auth_token,
-                     parsem3u8, webparse, webparse_m3u8)
+from .downloader import download_chunk, merge_video
+from .parser import get_parser
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'], ignore_unknown_options=True)
 
@@ -52,6 +51,23 @@ def cli(version=False, update=False):
         exit(0)
 
 
+# @cli.command("download", short_help="Download video from abema.tv")
+# @click.argument("input", metavar="<AbemaTV url site or m3u8>")
+# @click.option("--proxy", "-p", required=False, default=None, metavar="<ip:port/url>", help="Use http(s)/socks5 proxies (please add `socks5://` if you use socks5)")
+# @click.option("--resolution", "-r", "res", required=False, type=click.Choice(['180p', '240p', '360p', '480p', '720p', '1080p']), default="1080p", help="Resolution to be downloaded (Default: 1080p)")
+# @click.option("--resolutions", "-R", "resR", is_flag=True, help="Show available resolutions")
+# @click.option("--output", "-o", required=False, default=None, help="Output filename")
+# @click.option('--verbose', '-v', is_flag=True, help="Enable verbosity")
+# def main_downloaderv2(input, proxy, res, resR, output, verbose):
+#     print('[INFO] Starting yuu v{ver}...'.format(ver=__version__))
+
+#     upstream_data = requests.get("https://pastebin.com/raw/Bt3ZLjfu").json()
+#     upstream_version = upstream_data['version']
+#     if upstream_version != __version__:
+#         print('[INFO] There\'s new version available to download, please update using `yuu -U`.')
+#         exit(0)
+
+
 @cli.command("download", short_help="Download video from abema.tv")
 @click.argument("input", metavar="<AbemaTV url site or m3u8>")
 @click.option("--proxy", "-p", required=False, default=None, metavar="<ip:port/url>", help="Use http(s)/socks5 proxies (please add `socks5://` if you use socks5)")
@@ -63,47 +79,34 @@ def main_downloader(input, proxy, res, resR, output, verbose):
     """Download a free video from abema"""
     print('[INFO] Starting yuu v{ver}...'.format(ver=__version__))
 
-    upstream_data = requests.get("https://pastebin.com/raw/Bt3ZLjfu").json()
-    upstream_version = upstream_data['version']
-    if upstream_version != __version__:
-        print('[INFO] There\'s new version available to download, please update using `yuu -U`.')
-        exit(0)
+    #upstream_data = requests.get("https://pastebin.com/raw/Bt3ZLjfu").json()
+    #upstream_version = upstream_data['version']
+    #if upstream_version != __version__:
+    #    print('[INFO] There\'s new version available to download, please update using `yuu -U`.')
+    #    exit(0)
 
     sesi = requests.Session()
     if proxy:
         print('[INFO] Testing proxy')
-        sesi.proxies = {'http': proxy, 'https': proxy}
-        # Somebody tell me how to do recursive test properly
-        try:
-            if verbose:
-                print('[DEBUG] Testing http+https mode proxy')
-            sesi.get('http://httpbin.org/get') # Some test website to check if proxy works or not
-            pmode = "HTTP+HTTPS/SOCKS5"
-        except requests.exceptions.RequestException:
-            if verbose:
-                print('[DEBUG] Failed')
-            sesi = requests.Session()
-            sesi.proxies = {'http': proxy}
+        proxy_test = [
+            {'http': proxy, 'https': proxy},
+            {'https': proxy},
+            {'http': proxy}
+        ]
+        for mode in proxy_test:
             try:
                 if verbose:
-                    print('[DEBUG] Testing http mode proxy')
-                sesi.get('http://httpbin.org/get') # This too but in https mode
-                pmode = "HTTP/SOCKS5"
+                    print('Testing {x} mode proxy'.format(x="+".join(mode.keys())))
+                sesi.proxies = mode
+                sesi.get('http://httpbin.org/get') # Test website to check if proxy works or not
+                pmode = "+".join(mode.keys()).upper() + "/SOCKS5"
             except requests.exceptions.RequestException:
                 if verbose:
                     print('[DEBUG] Failed')
-                sesi = requests.Session()
-                sesi.proxies = {'https': proxy} # Final test if it's failed then it will return error
-                try:
-                    if verbose:
-                        print('[DEBUG] Testing https mode proxy')
-                    sesi.get('http://httpbin.org/get')
-                    pmode = "HTTPS/SOCKS5"
-                except requests.exceptions.RequestException:
-                    if verbose:
-                        print('[DEBUG] Failed')
+                if mode == proxy_test[-1]:
                     print('[ERROR] Cannot connect to proxy (Request timeout)')
                     exit(1)
+        sesi.proxies = {'http': proxy, 'https': proxy}
     try:
         sesi.get('http://httpbin.org/get')
         pmode = "No proxy"
@@ -113,67 +116,66 @@ def main_downloader(input, proxy, res, resR, output, verbose):
     if verbose:
         print('[DEBUG] Using proxy mode: {}'.format(pmode))
 
-    print('[INFO] Fetching user token')
-    authtoken = get_auth_token(sesi, verbose)
-    sesi.headers.update({'Authorization': authtoken[0]})
+    yuuParser = get_parser(input)
 
-    if input[-5:] != '.m3u8':
-        print('[INFO] Parsing webpage')
-        outputtitle, m3u8link = webparse(input, res, sesi, verbose)
-        if resR:
-            print('[INFO] Checking available resolution')
-            avares = available_resolution(m3u8link, sesi, verbose)
-            print('[INFO] Available resolution:')
-            print('{0: <{width}}{1: <{width}}{2: <{width}}{3: <{width}}'.format("", "Resolution", "Video Quality", "Audio Quality", width=16))
-            for res in avares:
-                r_c, wxh = res
-                vidq, audq = res_data[r_c]
-                print('{0: <{width}}{1: <{width}}{2: <{width}}{3: <{width}}'.format('>> ' + r_c, wxh, vidq, audq, width=16))
-            exit(0)
-        print('[INFO] Parsing m3u8')
-        files, iv, ticket = parsem3u8(m3u8link, res, sesi, verbose)
-        if output:
-            if output[-3:] == '.ts':
-                output = output
-            else:
-                output = output + '.ts'
+    if not yuuParser:
+        print('[ERROR] Unknown url')
+        exit(1)
+
+    yuuParser = yuuParser(sesi, verbose)
+
+    if 'AbemaTV' in yuuParser.type:
+        mode = 'AbemaTV'
+    elif 'GYAO' in yuuParser.type:
+        mode = 'GYAO'
+
+    print('[INFO] {}: Fetching user token'.format(mode))
+    yuuParser.get_token()
+
+    print('[INFO] {}: Parsing url'.format(mode))
+    output_name = yuuParser.parse(input, res)
+    if resR:
+        print('[INFO] {}: Checking available resolution'.format(mode))
+        avares = yuuParser.resolutions()
+        print('[INFO] {}: Available resolution:'.format(mode))
+        print('{0: <{width}}{1: <{width}}{2: <{width}}{3: <{width}}'.format("", "Resolution", "Video Quality", "Audio Quality", width=16))
+        for res in avares:
+            r_c, wxh = res
+            vidq, audq = res_data[r_c]
+            print('{0: <{width}}{1: <{width}}{2: <{width}}{3: <{width}}'.format('>> ' + r_c, wxh, vidq, audq, width=16))
+        exit(0)
+
+    if output:
+        if output[-3:] == '.ts':
+            output = output
         else:
-            output = '{x} (AbemaTV {r}).ts'.format(x=outputtitle, r=res)
-        if verbose:
-            print('[DEBUG] Output file: {}'.format(output))
+            output = output + '.ts'
     else:
-        print('[INFO] Parsing m3u8')
-        outputtitle, res = webparse_m3u8(input, sesi, verbose)
-        files, iv, ticket = parsem3u8(input, res, sesi, verbose)
-        if output:
-            if output[-3:] == '.ts':
-                output = output
-            else:
-                output = output + '.ts'
-        else:
-            output = '{x} (AbemaTV {r}).ts'.format(x=outputtitle, r=res)
+        output = '{x} (AbemaTV {r}).ts'.format(x=output_name, r=res)
 
-    # Don't use forbidden/illegal character (replace it with underscore)
+    print('[INFO] {}: Parsing m3u8'.format(mode))
+    files, iv = yuuParser.parse_m3u8()
+
     illegalchar = ['/', '<', '>', ':', '"', '\\', '|', '?', '*'] # https://docs.microsoft.com/en-us/windows/desktop/FileIO/naming-a-file
     for char in illegalchar:
         output = output.replace(char, '_')
 
-    print('[INFO] Fetching video key')
-    getkey = fetch_video_key(ticket, authtoken, sesi, verbose)
-    
-    print('[INFO][DOWN] Starting downloader...')
-    print('[INFO][DOWN] Resolution: {}'.format(res))
-    dllist, tempdir = get_video(files, getkey, iv, sesi, verbose)
-    if not dllist:
-        if tempdir:
-            shutil.rmtree(tempdir)
-        exit(0)
-    print('[INFO][DOWN] Finished downloading')
-    print('[INFO] Merging video')
-    merge_video(dllist, output)
+    print('[INFO] {}: Fetching video key'.format(mode))
+    video_key = yuuParser.get_video_key()#fetch_video_key(ticket, authtoken, sesi, verbose)
 
-    print('[INFO] Cleaning up')
-    shutil.rmtree(tempdir)
+    print('[INFO][DOWN] Starting downloader...')
+    print('[INFO][DOWN] Output: {}'.format(output))
+    print('[INFO][DOWN] Resolution: {}'.format(res))
+    dl_list, temp_dir = download_chunk(files, video_key, iv, yuuParser.session)
+    if not dl_list:
+        if temp_dir:
+            shutil.rmtree(temp_dir)
+        exit(1)
+    print('[INFO][DOWN] Finished downloading')
+    print('[INFO][DOWN] Merging video')
+    merge_video(dl_list, temp_dir)
+    shutil.rmtree(temp_dir)
+    print('[INFO] Finished downloading: {}'.format(output))
     exit(0)
 
 
