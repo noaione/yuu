f1b3ed36e1823ce9efe3efed0f58a9aeb2a9a33c
NoAiOne <aimanmh@my.smccd.edu>
2019-10-22 05:39:01 +0700

    Add mux support
    Former-commit-id: 640e7b9bb902d3ec7b4bb416dec2e6c7965a5661

--- yuu/common.py
+++ yuu/common.py
@@ -1,6 +1,7 @@
-import os
 import json
+import os
 import re
+import subprocess
 
 from tqdm import tqdm
 
@@ -10,6 +11,11 @@ __version__ = "1.0.0"
 
 
 def get_parser(url):
+    """
+    Function that are called first time to check if it's a valid supported link
+
+    :return: A class of one of supported website
+    """
     valid_abema = r'http(?:|s)://(?:abema\.tv)/(?:channels|video)/(?:\w*)(?:/|-\w*/)((?P<slot>slots/)|)(?P<video_id>.*[^-_])'
     valid_gyao = r'(?isx)http(?:|s)://gyao.yahoo.co.jp/(?:player|title[\w])/(?P<p1>[\w]*.*)'
     valid_aniplus = r'http(?:|s)://(?:www\.|)aniplus-asia\.com/episode/(?P<video_id>[\w]*.*)'
@@ -23,6 +29,9 @@ def get_parser(url):
 
 
 def merge_video(path, output):
+    """
+    Merge every video chunk to a single file output
+    """
     with open(output, 'wb') as out:
         with tqdm(total=len(path), desc="Merging", ascii=True, unit="file") as pbar:
             for i in path:
@@ -30,20 +39,33 @@ def merge_video(path, output):
                 os.remove(i)
                 pbar.update()
 
-"""
-Admin Check Code from https://gist.github.com/sylvainpelissier/ff072a6759082590a4fe8f7e070a4952
-"""
-def isUserAdmin():
-    if os.name == 'nt':
-        import ctypes
-        # WARNING: requires Windows XP SP2 or higher!
+
+def mux_video(old_file):
+    """
+    Mux .ts or .mp4 or anything to a .mkv
+
+    It will try to use ffmpeg first, if it's not in the PATH, then it will try to use mkvmerge
+    If it's doesn't exist too, it just gonna skip.
+    """
+    # FFMPEG check
+    use_ffmpeg = False
+    use_mkvmerge = False
+    try:
+        subprocess.check_call(['ffmpeg', '-version'])
+        use_ffmpeg = True
+    except FileNotFoundError:
         try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
-            print("Admin check failed, assuming not an admin.")
-            return False
-    else:
-        return os.getuid() == 0
+            subprocess.check_call(['mkvmerge', '-V'])
+            use_mkvmerge = True
+        except FileNotFoundError:
+            return None
+
+    fn_, _ = os.path.splitext(old_file)
+    if use_ffmpeg:
+        subprocess.call(['ffmpeg', '-i', old_file, '-c', 'copy', '{f}.mkv'.format(f=fn_)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if use_mkvmerge:
+        subprocess.call(['mkvmerge', '-o', '{f}.mkv'.format(f=fn_), old_file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return '{f}.mkv'.format(f=fn_)
 
 
 def read_yuu_data(path):
